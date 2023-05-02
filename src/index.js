5ac2ffa08ce4c623e374cac7239566e0f322df98
gusttavares97 <gustavotavares018@gmail.com>
2023-05-02 19:14:26 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as ServiceWorker from './serviceWorker'
+
+
+ReactDOM.render(
+
+
+  document.getElementById('root')
+)
+
+ServiceWorker.unregister();
